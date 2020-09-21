883e6b06933279f47ceff11536a1d134a6089026
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-21 16:06:40 +0100

    JWT FILTER & UPON SUCCESSFUL AUTHENTICATION ==>>> Generate and Send Token back to user. closes #23

--- src/main/java/com/zikozee/all_spring_security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/zikozee/all_spring_security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,18 +1,26 @@
 package com.zikozee.all_spring_security.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 //THIS HANDLES CREDENTIAL SENT AND VALIDATION OF THE CREDENTIALS
 
+@Slf4j
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
@@ -41,5 +49,28 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
             throw new RuntimeException(e);
         }
 
+    }
+
+    //THIS METHOD IS INVOKED AFTER attemptAuthentication Above is successful else this method never gets executed
+    //Here we generate the token and send to the client
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        log.info("IS USERNAME CORRECT::-->> " + authResult.getName());
+
+        String key = "securesecuresecuresecuresecuresecuresecuresecuresecuresecuresecure"; // another idea keep key on a different server e.g another machine or Firebase. as JSON n read it with JSON SIMPLE
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())//this is supposed to return usernames in DB, if USER is returned use (User)authResult.getPrincipal(), assign to a variable user and do user.getUsername()
+                .claim("authorities", authResult.getAuthorities())// body or Payload data
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2))) //duration
+                .signWith(Keys.hmacShaKeyFor(key.getBytes())) //signature
+                .compact();
+
+        //sending token back
+        response.setHeader("Authorization", "Bearer " + token);
+
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
